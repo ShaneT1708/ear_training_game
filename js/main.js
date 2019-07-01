5694271cea40609ef76b9144a126f96c1222611b
shane <shanethanner@gmail.com>
2019-07-01 14:52:17 +0100

    Audio Playback Added

--- js/main.js
+++ js/main.js
@@ -9,32 +9,34 @@ $(document).ready(function () {
     // SOUNDS
 
 
-    var c3 = new Audio('assets/notes/c3');
-    var cs3 = new Audio('assets/notes/cs3');
-    var d3 = new Audio('assets/notes/d3');
-    var ds3 = new Audio('assets/notes/ds3');
-    var e3 = new Audio('assets/notes/e3');
-    var f3 = new Audio('assets/notes/f3');
-    var fs3 = new Audio('assets/notes/fs3');
-    var g3 = new Audio('assets/notes/g3');
-    var gs3 = new Audio('assets/notes/gs3');
-    var a3 = new Audio('assets/notes/a3');
-    var as3 = new Audio('assets/notes/as3');
-    var b3 = new Audio('assets/notes/b3');
-    var c4 = new Audio('assets/notes/c4');
-    var cs4 = new Audio('assets/notes/cs4');
-    var d4 = new Audio('assets/notes/d4');
-    var ds4 = new Audio('assets/notes/ds4');
-    var e4 = new Audio('assets/notes/e4');
-    var f4 = new Audio('assets/notes/f4');
-    var fs4 = new Audio('assets/notes/fs4');
-    var g4 = new Audio('assets/notes/g4');
-    var gs4 = new Audio('assets/notes/gs4');
-    var a4 = new Audio('assets/notes/a4');
-    var as4 = new Audio('assets/notes/as4');
-    var b4 = new Audio('assets/notes/b4');
-    var c5 = new Audio('assets/notes/c5');
+    var c3 = new Audio('assets/notes/c3.mp3');
+    var cs3 = new Audio('assets/notes/cs3.mp3');
+    var d3 = new Audio('assets/notes/d3.mp3');
+    var ds3 = new Audio('assets/notes/ds3.mp3');
+    var e3 = new Audio('assets/notes/e3.mp3');
+    var f3 = new Audio('assets/notes/f3.mp3');
+    var fs3 = new Audio('assets/notes/fs3.mp3');
+    var g3 = new Audio('assets/notes/g3.mp3');
+    var gs3 = new Audio('assets/notes/gs3.mp3');
+    var a3 = new Audio('assets/notes/a3.mp3');
+    var as3 = new Audio('assets/notes/as3.mp3');
+    var b3 = new Audio('assets/notes/b3.mp3');
+    var c4 = new Audio('assets/notes/c4.mp3');
+    var cs4 = new Audio('assets/notes/cs4.mp3');
+    var d4 = new Audio('assets/notes/d4.mp3');
+    var ds4 = new Audio('assets/notes/ds4.mp3');
+    var e4 = new Audio('assets/notes/e4.mp3');
+    var f4 = new Audio('assets/notes/f4.mp3');
+    var fs4 = new Audio('assets/notes/fs4.mp3');
+    var g4 = new Audio('assets/notes/g4.mp3');
+    var gs4 = new Audio('assets/notes/gs4.mp3');
+    var a4 = new Audio('assets/notes/a4.mp3');
+    var as4 = new Audio('assets/notes/as4.mp3');
+    var b4 = new Audio('assets/notes/b4.mp3');
+    var c5 = new Audio('assets/notes/c5.mp3');
   
+    var notes = [c3,cs3,d3,ds3,e3,f3,fs3,g3,gs3,a3,as3,b3,c4,cs4,d4,ds4,e4,f4,fs4,g4,gs4,a4,as4,b4,c5];
+
 
 
 
@@ -50,21 +52,22 @@ $(document).ready(function () {
         $("#tempSlideInfo").text(`BPM: ${document.getElementById("tempSlide").value}`);
     })
 
+    
     var melody = [];
 
     // When "Play!" is clicked
 
     $("#play").on("click", function () {
-
+        var j = 0;
         melody.length = 0;
         var melLength = document.getElementById("lenSlide").value;
+        var tempo = document.getElementById("tempSlide").value;
         for (i = 0; i < melLength; i++) {
             melody.push(Math.floor(Math.random() * 19));
         }
 
-        setTimeout(melodyPlayback(melody), 1000);
+        setTimeout(melodyPlayback, 1000,melody,notes,tempo,j);
         console.log(melody);
-
     })
 
     //MAIN GAME
@@ -108,7 +111,32 @@ function compare(arr1, arr2) {
 }
 
 // Playback Melody Array
+//WRITE ABOUT WHY FOR LOOP DIDN'T WORK IN READ.ME
 
-function melodyPlayback(mel) {
-    $()
-}
+
+function melodyPlayback(mel,nots,bpm,jj) {
+    
+    console.log(bpm);
+    setTimeout(playNote,(60000/bpm),mel,nots,bpm,jj);
+
+
+   /* for(var i =0;i<mel.length;i++){
+        //setTimeout(nots[i].play(),1000);
+      // setTimeout(console.log(nots[i]),1000);
+      setTimeout(function(){nots[i].play();},1000);
+      console.log(i);
+    } */
+};
+
+//Play Note
+
+function playNote(mel,nots,bpm,jj){
+    console.log(jj);
+    if(mel[jj]==mel[jj-1]){
+        nots[mel[jj-1]].load();
+    }
+    nots[mel[jj]].play();
+    jj++;
+    if(jj<mel.length){
+        melodyPlayback(mel,nots,bpm,jj);
+    }};
